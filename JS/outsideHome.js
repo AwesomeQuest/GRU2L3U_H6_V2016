e2b0f3aeac8d086b0dd9243a16a5301da5ca7683
AwesomeQuest <torfit@gmail.com>
2016-05-03 10:46:09 +0000

    Added JS code

--- JS/outsideHome.js
+++ JS/outsideHome.js
@@ -1,2 +1,4 @@
-
-document.getElementsByClassName(game).style.backgroundimage = "url('C:/Users/torfi/Documents/GitHub/GRU2L3U_H6_V2016/Things/OutsideOfHouse.png')";
+function loadImg() {
+	document.getElementsById('bgr').scr = "C:/Users/torfi/Documents/GitHub/GRU2L3U_H6_V2016/Things/OutsideOfHouse.png";
+    alert("Page is loaded");
+}
